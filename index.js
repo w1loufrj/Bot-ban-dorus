68966e77c523306c19119059cb624ce64e16046b
Elouan Blondel <214812255+w1loufrj@users.noreply.github.com>
2026-08-08 09:18:32 +0200

    Refactor Discord bot setup and remove unused code

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const dns = require('dns');
-dns.setDefaultResultOrder('ipv4first');
-
-
 const { Client, GatewayIntentBits, ChannelType } = require('discord.js');
-const http = require('http');
-const https = require('https');
+const dns = require('dns');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Bot en ligne');
-});
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Serveur web de ping démarré');
-});
+dns.setDefaultResultOrder('ipv4first');
 
 const client = new Client({ 
   intents: [
@@ -22,35 +11,9 @@ const client = new Client({
   ] 
 });
 
-client.on('error', (err) => {
-  console.error('ERREUR CLIENT:', err.message);
-});
-
-client.on('shardError', (err) => {
-  console.error('ERREUR SHARD:', err.message);
-});
-
-client.on('shardDisconnect', (event, id) => {
-  console.log('SHARD DÉCONNECTÉ:', event.code, event.reason);
-});
-
-client.on('shardReconnecting', (id) => {
-  console.log('SHARD RECONNEXION...');
-});
-
-client.on('debug', (info) => {
-  console.log('DEBUG:', info);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.error('REJECTION NON GÉRÉE:', err);
-});
-
 const SALONS_PIEGE_IDS = ['1526206401561497670'];
 
 client.on('messageCreate', async (message) => {
-  console.log(`Message reçu dans le salon ${message.channel.id} par ${message.author.tag}`);
-
   if (!SALONS_PIEGE_IDS.includes(message.channel.id)) return;
   if (message.author.bot) return;
 
@@ -92,7 +55,6 @@ client.once('ready', () => {
   console.log(`Bot connecté en tant que ${client.user.tag}`);
 });
 
-console.log('Tentative de connexion à Discord...');
 client.login(process.env.BOT_TOKEN).catch((err) => {
   console.error('ERREUR DE CONNEXION:', err.message);
 });
